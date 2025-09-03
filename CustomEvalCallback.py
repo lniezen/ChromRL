46113393403753b59a95955bcdd52b0bc329ab07
lniezen <155958429+lniezen@users.noreply.github.com>
2025-09-03 15:09:08 +0200

    Update CustomEvalCallback.py

--- CustomEvalCallback.py
+++ CustomEvalCallback.py
@@ -50,94 +50,93 @@ class CustomEvalCallback(EvalCallback):
     """
 
     def _on_step(self) -> bool:
-        continue_training = True
+    continue_training = True
 
-        if self.eval_freq > 0 and self.n_calls % self.eval_freq == 0:
-            # Sync training and eval env if there is VecNormalize
-            print("evaluating")
-            if self.model.get_vec_normalize_env() is not None:
-                try:
-                    sync_envs_normalization(self.training_env, self.eval_env)
-                except AttributeError as e:
-                    raise AssertionError(
-                        "Training and eval env are not wrapped the same way, "
-                        "see https://stable-baselines3.readthedocs.io/en/master/guide/callbacks.html#evalcallback "
-                        "and warning above."
-                    ) from e
+    if self.eval_freq > 0 and self.n_calls % self.eval_freq == 0:
+        # Sync training and eval env if there is VecNormalize
+        if self.model.get_vec_normalize_env() is not None:
+            try:
+                sync_envs_normalization(self.training_env, self.eval_env)
+            except AttributeError as e:
+                raise AssertionError(
+                    "Training and eval env are not wrapped the same way, "
+                    "see https://stable-baselines3.readthedocs.io/en/master/guide/callbacks.html#evalcallback "
+                    "and warning above."
+                ) from e
 
-            # Reset success rate buffer
-            self._is_success_buffer = []
-            episode_rewards, episode_lengths, episode_solutions, episode_variances = custom_evaluate_policy(
-                self.model,
-                self.eval_env,
-                n_eval_episodes=self.n_eval_episodes,
-                render=self.render,
-                deterministic=self.deterministic,
-                return_episode_rewards=True,
-                warn=self.warn,
-                callback=self._log_success_callback,
+        # Reset success rate buffer
+        self._is_success_buffer = []
+
+        episode_rewards, episode_lengths, episode_solutions = custom_evaluate_policy(
+            self.model,
+            self.eval_env,
+            n_eval_episodes=self.n_eval_episodes,
+            render=self.render,
+            deterministic=self.deterministic,
+            return_episode_rewards=True,
+            warn=self.warn,
+            callback=self._log_success_callback,
+        )
+
+        if self.log_path is not None:
+            assert isinstance(episode_rewards, list)
+            assert isinstance(episode_lengths, list)
+            self.evaluations_timesteps.append(self.num_timesteps)
+            self.evaluations_results.append(episode_rewards)
+            self.evaluations_length.append(episode_lengths)
+
+            kwargs = {}
+            # Save success log if present
+            if len(self._is_success_buffer) > 0:
+                self.evaluations_successes.append(self._is_success_buffer)
+                kwargs = dict(successes=self.evaluations_successes)
+
+            np.savez(
+                self.log_path,
+                timesteps=self.evaluations_timesteps,
+                results=self.evaluations_results,
+                ep_lengths=self.evaluations_length,
+                **kwargs,  # type: ignore[arg-type]
             )
 
-            if self.log_path is not None:
-                assert isinstance(episode_rewards, list)
-                assert isinstance(episode_lengths, list)
-                self.evaluations_timesteps.append(self.num_timesteps)
-                self.evaluations_results.append(episode_rewards)
-                self.evaluations_length.append(episode_lengths)
+        mean_reward, std_reward = np.mean(episode_rewards), np.std(episode_rewards)
+        mean_ep_length, std_ep_length = np.mean(episode_lengths), np.std(episode_lengths)
+        mean_solved, std_solved = np.mean(episode_solutions), np.std(episode_solutions)
 
-                kwargs = {}
-                # Save success log if present
-                if len(self._is_success_buffer) > 0:
-                    self.evaluations_successes.append(self._is_success_buffer)
-                    kwargs = dict(successes=self.evaluations_successes)
+        self.last_mean_reward = float(mean_reward)
 
-                np.savez(
-                    self.log_path,
-                    timesteps=self.evaluations_timesteps,
-                    results=self.evaluations_results,
-                    ep_lengths=self.evaluations_length,
-                    **kwargs,  # type: ignore[arg-type]
-                )
+        if self.verbose >= 1:
+            print(f"Eval num_timesteps={self.num_timesteps}, " f"episode_reward={mean_reward:.2f} +/- {std_reward:.2f}")
+            print(f"Episode length: {mean_ep_length:.2f} +/- {std_ep_length:.2f}")
+            print(f"Fraction solved samples: {mean_solved:.2f} +/- {std_solved:.2f}")
+        # Add to current Logger
+        self.logger.record("eval/mean_reward", float(mean_reward))
+        self.logger.record("eval/mean_ep_length", mean_ep_length)
+        self.logger.record("eval/mean_solved", mean_solved)
 
-            mean_reward, std_reward = np.mean(episode_rewards), np.std(episode_rewards)
-            mean_ep_length, std_ep_length = np.mean(episode_lengths), np.std(episode_lengths)
-            mean_solved, std_solved = np.mean(episode_solutions), np.std(episode_solutions)
-            mean_tr_variance, std_tr_variance = np.mean(episode_variances), np.std(episode_variances)
-
-            self.last_mean_reward = float(mean_reward)
-
+        if len(self._is_success_buffer) > 0:
+            success_rate = np.mean(self._is_success_buffer)
             if self.verbose >= 1:
-                print(f"Eval num_timesteps={self.num_timesteps}, " f"episode_reward={mean_reward:.2f} +/- {std_reward:.2f}")
-                print(f"Episode length: {mean_ep_length:.2f} +/- {std_ep_length:.2f}")
-                print(f"Fraction solved samples: {mean_solved:.2f} +/- {std_solved:.2f}")
-            # Add to current Logger
-            self.logger.record("eval/mean_reward", float(mean_reward))
-            self.logger.record("eval/mean_ep_length", mean_ep_length)
-            self.logger.record("eval/mean_solved", mean_solved)
-            self.logger.record("eval/mean_variance_tR", mean_tr_variance)
+                print(f"Success rate: {100 * success_rate:.2f}%")
+            self.logger.record("eval/success_rate", success_rate)
 
-            if len(self._is_success_buffer) > 0:
-                success_rate = np.mean(self._is_success_buffer)
-                if self.verbose >= 1:
-                    print(f"Success rate: {100 * success_rate:.2f}%")
-                self.logger.record("eval/success_rate", success_rate)
+        # Dump log so the evaluation results are printed with the correct timestep
+        self.logger.record("time/total_timesteps", self.num_timesteps, exclude="tensorboard")
+        self.logger.dump(self.num_timesteps)
 
-            # Dump log so the evaluation results are printed with the correct timestep
-            self.logger.record("time/total_timesteps", self.num_timesteps, exclude="tensorboard")
-            self.logger.dump(self.num_timesteps)
+        if mean_reward > self.best_mean_reward:
+            if self.verbose >= 1:
+                print("New best mean reward!")
+            if self.best_model_save_path is not None:
+                self.model.save(os.path.join(self.best_model_save_path, "best_model"))
+            self.best_mean_reward = float(mean_reward)
+            # Trigger callback on new best model, if needed
+            if self.callback_on_new_best is not None:
+                continue_training = self.callback_on_new_best.on_step()
 
-            if mean_reward > self.best_mean_reward:
-                if self.verbose >= 1:
-                    print("New best mean reward!")
-                if self.best_model_save_path is not None:
-                    self.model.save(os.path.join(self.best_model_save_path, "best_model"))
-                self.best_mean_reward = float(mean_reward)
-                # Trigger callback on new best model, if needed
-                if self.callback_on_new_best is not None:
-                    continue_training = self.callback_on_new_best.on_step()
+        # Trigger callback after every evaluation, if needed
+        if self.callback is not None:
+            continue_training = continue_training and self._on_event()
 
-            # Trigger callback after every evaluation, if needed
-            if self.callback is not None:
-                continue_training = continue_training and self._on_event()
+    return continue_training
 
-        return continue_training
