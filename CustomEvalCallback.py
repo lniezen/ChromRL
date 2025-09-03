9803b7986cd0606ea478a59ee1a0c546b93b9bbd
lniezen <155958429+lniezen@users.noreply.github.com>
2025-09-03 15:13:42 +0200

    Update CustomEvalCallback.py

--- CustomEvalCallback.py
+++ CustomEvalCallback.py
@@ -48,10 +48,10 @@ class CustomEvalCallback(EvalCallback):
     :param warn: Passed to ``evaluate_policy`` (warns if ``eval_env`` has not been
         wrapped with a Monitor wrapper)
     """
-
+    
     def _on_step(self) -> bool:
         continue_training = True
-    
+
         if self.eval_freq > 0 and self.n_calls % self.eval_freq == 0:
             # Sync training and eval env if there is VecNormalize
             if self.model.get_vec_normalize_env() is not None:
@@ -63,10 +63,10 @@ class CustomEvalCallback(EvalCallback):
                         "see https://stable-baselines3.readthedocs.io/en/master/guide/callbacks.html#evalcallback "
                         "and warning above."
                     ) from e
-    
+
             # Reset success rate buffer
             self._is_success_buffer = []
-    
+
             episode_rewards, episode_lengths, episode_solutions = custom_evaluate_policy(
                 self.model,
                 self.eval_env,
@@ -77,20 +77,20 @@ class CustomEvalCallback(EvalCallback):
                 warn=self.warn,
                 callback=self._log_success_callback,
             )
-    
+
             if self.log_path is not None:
                 assert isinstance(episode_rewards, list)
                 assert isinstance(episode_lengths, list)
                 self.evaluations_timesteps.append(self.num_timesteps)
                 self.evaluations_results.append(episode_rewards)
                 self.evaluations_length.append(episode_lengths)
-    
+
                 kwargs = {}
                 # Save success log if present
                 if len(self._is_success_buffer) > 0:
                     self.evaluations_successes.append(self._is_success_buffer)
                     kwargs = dict(successes=self.evaluations_successes)
-    
+
                 np.savez(
                     self.log_path,
                     timesteps=self.evaluations_timesteps,
@@ -98,13 +98,13 @@ class CustomEvalCallback(EvalCallback):
                     ep_lengths=self.evaluations_length,
                     **kwargs,  # type: ignore[arg-type]
                 )
-    
+
             mean_reward, std_reward = np.mean(episode_rewards), np.std(episode_rewards)
             mean_ep_length, std_ep_length = np.mean(episode_lengths), np.std(episode_lengths)
             mean_solved, std_solved = np.mean(episode_solutions), np.std(episode_solutions)
-    
+
             self.last_mean_reward = float(mean_reward)
-    
+
             if self.verbose >= 1:
                 print(f"Eval num_timesteps={self.num_timesteps}, " f"episode_reward={mean_reward:.2f} +/- {std_reward:.2f}")
                 print(f"Episode length: {mean_ep_length:.2f} +/- {std_ep_length:.2f}")
@@ -113,17 +113,17 @@ class CustomEvalCallback(EvalCallback):
             self.logger.record("eval/mean_reward", float(mean_reward))
             self.logger.record("eval/mean_ep_length", mean_ep_length)
             self.logger.record("eval/mean_solved", mean_solved)
-    
+
             if len(self._is_success_buffer) > 0:
                 success_rate = np.mean(self._is_success_buffer)
                 if self.verbose >= 1:
                     print(f"Success rate: {100 * success_rate:.2f}%")
                 self.logger.record("eval/success_rate", success_rate)
-    
+
             # Dump log so the evaluation results are printed with the correct timestep
             self.logger.record("time/total_timesteps", self.num_timesteps, exclude="tensorboard")
             self.logger.dump(self.num_timesteps)
-    
+
             if mean_reward > self.best_mean_reward:
                 if self.verbose >= 1:
                     print("New best mean reward!")
@@ -133,11 +133,10 @@ class CustomEvalCallback(EvalCallback):
                 # Trigger callback on new best model, if needed
                 if self.callback_on_new_best is not None:
                     continue_training = self.callback_on_new_best.on_step()
-    
+
             # Trigger callback after every evaluation, if needed
             if self.callback is not None:
                 continue_training = continue_training and self._on_event()
-    
+
         return continue_training
-
-
+        
